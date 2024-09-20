4bf24d0216adb469d52228f05d3c56df52da45af
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:37 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libgcryptport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libgcryptport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgcryptport.git'), string(name: 'PORT_DESCRIPTION', value: 'Libgcrypt is a general purpose cryptographic library originally based on code from GnuPG.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libgcryptport.git'), string(name: 'PORT_DESCRIPTION', value: 'Libgcrypt is a general purpose cryptographic library originally based on code from GnuPG.' )]
   }
 }
